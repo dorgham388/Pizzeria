dd909b035737a3736d275598c23dcea1b73f0baf
Amir Dorgham <dorgham388@gmail.com>
2021-05-12 05:25:29 +0200

    dark mode support

--- src/routes/publicRoute.js
+++ src/routes/publicRoute.js
@@ -2,20 +2,26 @@
  * Created Date: Tuesday May 11th 2021
  * Author: Amir Dorgham
  * -----
- * Last Modified: Tuesday, May 11th 2021, 4:37:53 pm
+ * Last Modified: Wednesday, May 12th 2021, 5:15:11 am
  * Modified By: Amir Dorgham
  * -----
  */
 
 import React from "react";
 import { Route } from "react-router-dom";
-
+import { ThemeProvider } from "utils/theme";
 const PublicRoute = ({ component: Component, ...rest }) => {
   return (
     <Route
       {...rest}
       render={(props) => (
-        <React.Fragment>{<Component {...props} />}</React.Fragment>
+        <React.Fragment>
+          {
+            <ThemeProvider>
+              <Component {...props} />
+            </ThemeProvider>
+          }
+        </React.Fragment>
       )}
     />
   );
--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 // import React, { useState } from "react";
 // import { useSelector, useDispatch } from "react-redux";
+import tw from "twin.macro";
+import Toggle from "components/theme/toggle";
+const Container = tw.div`bg-gray-900 dark:bg-yellow-300 min-h-screen`;
 
 export default function Home() {
   // const count = useSelector((state) => state);
   // const dispatch = useDispatch();
   // const [state, setState] = useState({});
 
-  return <div>YA DALI YA MREKTI YA MTALLAH ZAYAYAY</div>;
+  return (
+    <Container>
+      <Toggle></Toggle>
+    </Container>
+  );
 }
